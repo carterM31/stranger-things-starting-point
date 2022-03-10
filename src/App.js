3753a0fbcb6bbc4776f8e5f898ccbb78d97294e3
Carter Marchbank <cartermarchbank33@gmail.com>
2022-03-09 21:25:56 -0600

    Updated PostForm.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import PostForm from "./PostForm";
 import PostList from "./PostList";
 
 // This is a test!!! Carter's Git repo push comment!
@@ -9,8 +10,13 @@ const App = (props) => {
   const { name } = props;
   return (
     <>
+      <div>
+        <h1>Hot Reload is working?</h1>
+      </div>
       <h1>Welcome, {name}</h1>
+      <PostForm />
       <PostList />
+      
     </>
   );
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,28 @@
 export const getPosts = async () => {
+    // The URL that we are reaching out to get.
     const url = 'https://jsonplaceholder.typicode.com/posts/';
-    const response = await fetch(url)
-    const json = await response.json()
+
+    // Get the body of given API 
+    const response = await fetch(url);
+    console.log(response);
+
+    const json = await response.json();
+    console.log(json);
+
     return json;
-} 
+}
+
+export const createNewPost = async (newPost) => {
+    const url = 'https://jsonplaceholder.typicode.com/posts/';
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newPost)
+    });
+    
+    const json = await response.json();
+    console.log(json);
+    return json;
+}
